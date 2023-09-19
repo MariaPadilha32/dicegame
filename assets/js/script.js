62cf6cd6a42e9c70bf3b8f9d16c5d8d7b7a45009
MariaPadilha32 <>
2023-09-19 20:50:14 +0000

    Add some css styling and add some javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,8 @@
-// Array of happiness quotes
-const happinessQuotes = [
-    "Happiness is a choice.",
-    "Find joy in the ordinary.",
-    "Smile, and the world smiles with you."
-];
+document.addEventListener("DOMContentLoaded", function () {
+    const happyQuotes = ["Item1", "Item2", "Item3", "Item4", "Item5"];
+    const randomIndex = Math.floor(Math.random() * happyQuotes.length);
+    const randomQuote = happyQuotes[randomIndex];
 
-// Function to generate a random quote
-function generateRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * happinessQuotes.length);
-    const randomQuote = happinessQuotes[randomIndex];
-    const quoteDisplay = document.getElementById("quote-display");
-    quoteDisplay.textContent = randomQuote;
-}
-
-// Attach the generateRandomQuote function to the button's click event
-const generateButton = document.getElementById("generate-quote");
-generateButton.addEventListener("click", generateRandomQuote);
-
-// Initial quote generation when the page loads
-generateRandomQuote();
+    const outputParagraph = document.getElementById("output-paragraph");
+    outputParagraph.textContent = randomQuote;
+});
